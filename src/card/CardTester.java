fe5d41ad7dc5a003addd8ef367240f8d9bf110dc
Sheetal <mehtasheetal7987@gmail.com>
2019-01-18 15:50:53 -0500

    1st Commit-Clone Demo

---
+++ src/card/CardTester.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package card;
+
+/**
+ *
+ * @author mehta
+ */
+public class CardTester {
+    public static void main(String[] args)
+    {
+        Card c1=new Card("Hearts",7);
+        c1.setSuit("Spades");
+        
+    }
+}
